8195f19fbfc65cbadc33129c9c32e5e0969b523e
Cleiton jr <cleitonjr.dev@gmail.com>
2022-10-26 13:59:09 -0300

    commite ate a aula 10 spinner

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -11,7 +11,7 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   // coursesService: CoursesService;
@@ -20,7 +20,7 @@ export class CoursesComponent implements OnInit {
     // this.courses = [];
     // this.coursesService = new CoursesService();
 
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
    }
 
   ngOnInit(): void {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, first } from 'rxjs/operators';
+import { tap, first, delay } from 'rxjs/operators';
 
 import { Course } from './../model/course';
 
@@ -18,6 +18,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
+      delay(5000),
       tap( courses => console.log(courses))
     );
   }
