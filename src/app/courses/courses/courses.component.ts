64dcb051026a599b7bfce1e1f1130ea908e0708a
Cleiton jr <cleitonjr.dev@gmail.com>
2022-10-27 11:37:41 -0300

    commit até a aula 12

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Course } from '../model/course';
 import { CoursesService } from './../services/courses.service';
@@ -16,12 +18,26 @@ export class CoursesComponent implements OnInit {
 
   // coursesService: CoursesService;
 
-  constructor( private coursesService: CoursesService ) {
-    // this.courses = [];
-    // this.coursesService = new CoursesService();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
 
-    this.courses$ = this.coursesService.list();
+
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError( error => {
+        this.onError('Erro ao carregar cursos.');
+        return of([])
+      })
+    );
    }
+
+   onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
 
   ngOnInit(): void {
 
